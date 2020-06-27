c752b1ce5620ff979de5a86c8617b9f9424b28e2
nikotbitw99 <51329702+nikotbitw99@users.noreply.github.com>
2020-06-26 21:22:54 -0400

    hols
    version 1.0
    
    Co-Authored-By: nikkybuckets10 <nikkybuckets10@users.noreply.github.com>

---
+++ GestionDeEncomiendas/src/gestiondeencomiendas/GestionDeEncomiendas.java
@@ -0,0 +1,14 @@
+
+package gestiondeencomiendas;
+
+
+public class GestionDeEncomiendas {
+
+  
+    public static void main(String[] args) {
+       
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
+    }
+    
+}
